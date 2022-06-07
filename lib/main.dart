33bfa92ac09b12124bc8a52c1d581ee9c6559783
Rajat Verma <rajat.verma78402@gmail.com>
2022-06-07 19:28:45 +0530

    Can add new expenses now

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:expences/widgets/transactions_list.dart';
+import 'package:expences/widgets/user_transactions.dart';
+import 'package:flutter/material.dart';
+import './models/transaction.dart';
+import './widgets/transactions_list.dart';
+import './widgets/new_transactions.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  // late String titleInput;
+  // late String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Card(
+            color: Colors.blue,
+            child: Container(
+              width: double.infinity,
+              child: Text('chart!'),
+            ),
+            elevation: 5,
+          ),
+          UserTransactions(),
+        ],
+      ),
+    );
+  }
+}
